f3e83fe6446e7acb3903b806f41b2858ce15f110
rafael sanchez port <rafysanchez@hotmail.com>
2024-02-23 12:11:49 -0300

    Atualizações de código para corrigir erros e melhorar o desempenho.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -49,3 +49,63 @@
   }
 }
 </style>
+
+<script>
+
+import DetailView from './DetailView.vue';
+import { properties } from '../assets/db';
+import SearchProperty from '../components/icons/SearchProperty.vue';
+
+export default {
+  data() {
+    return {
+      count: 1,
+      propiedades: [],
+      showDetailView: false,
+      selectedProperty: null,
+      searchModel: {
+        beds: 1,
+        bath: 1,
+        park: 1,
+        price: 250000
+
+      }
+    };
+  },
+  components() {
+    DetailView;
+    SearchProperty;
+  },
+  methods: {
+    ShowDetail(property) {
+      this.selectedProperty = property;
+      this.showDetailView = true;
+    },
+    loadPropriedades() {
+      this.propiedades = properties;
+    },
+    filterProps() {
+      // Extract the filter criteria from the form fields
+      const { beds, bath, park, price } = this.searchModel;
+
+      // Use the `filter` method to filter the properties based on the criteria
+      this.propiedades = properties.filter((property) => {
+        return (
+          property.beds >= beds &&     // Adjusted property names to lowercase
+          property.bath >= bath &&
+          property.park >= park &&
+          property.Value <= price    // Assuming "Value" is the price property
+        );
+      });
+    },
+
+    updateValue(event) {
+      this.price = event.target.value;
+    }
+  },
+  mounted() {
+    this.loadPropriedades();
+  },
+
+}
+</script>
